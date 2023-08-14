069e236f7a0c742e780763102308304c732c4a28
user635 <berkayalpar1@gmail.com>
2023-08-14 20:24:15 +0300

    game finishes properly when the game resulted

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -9,17 +9,24 @@ while user == "rock" or user == "paper" or user == "scissors":
     match user, bot:
         case "rock", "paper":
             print("bot won")
+            break
         case "paper", "scissors":
             print("bot won")
+            break
         case "scissors", "rock":
             print("bot won")
+            break
         case "rock", "scissors":
             print("user won")
+            break
         case "paper", "rock":
             print("user won")
+            break
         case "scissors", "paper":
             print("user won")
+            break
         case _:
             print("draw")
+            break
 else:
     print('enter "rock", "paper" or "scissors"')
