6ddf8f7073c6c9249bb78072e405e4c73d2547c7
user635 <berkayalpar1@gmail.com>
2023-08-16 08:13:08 +0300

    if condition has been replaced with match-case

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -5,13 +5,15 @@ choices_list = ["rock", "paper", "scissors"]
 user: str = input("enter 'rock' for rock, 'scissors' for scissors and 'paper' for paper: ")
 bot = random.choice(choices_list)
 
-if user != "rock" and user != "paper" and user != "scissors":
+player_status: list = [user, bot]
+
+try:
+    match player_status:
+        case ["rock", "paper"] | ["paper", "scissors"] | ["scissors", "rock"]:
+            print(f"bot won \nuser: {user} \nbot: {bot}")
+        case ["paper", "rock"] | ["scissors", "paper"] | ["rock", "scissors"]:
+            print(f"user won \nuser: {user} \nbot: {bot}")
+        case _:
+            print(f"draw \nuser: {user} \nbot: {bot}")
+except ValueError:
     print('enter "rock", "paper" or "scissors"')
-else:
-    player_status: list = [user, bot]
-    if player_status  == ["rock", "paper"] or player_status  == ["paper", "scissors"] or player_status  == ["scissors", "rock"]:
-        print("bot won")
-    elif player_status == ["paper", "rock"] or player_status  == ["scissors", "paper"] or player_status  == ["rock", "scissors"]:
-        print("user won")
-    else:
-        print("draw")
